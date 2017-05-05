1d5c42f431d9b8e9fded8b36e9e442a750893b69
bin lu <lu.bin.lu.bin@gmail.com>
2017-05-05 14:18:00 -0400

    init

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// require('./index.css');
+// import App = require('./components/app');
+
+ReactDOM.render(
+  <div>{'h'}</div>,
+  document.getElementById('app')
+);
