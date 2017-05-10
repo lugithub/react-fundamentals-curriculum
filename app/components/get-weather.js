be8467fc7f538add15e460a2a58cb54462794344
bin lu <lu.bin.lu.bin@gmail.com>
2017-05-10 12:53:03 -0400

    step 4 open weather

--- app/components/get-weather.js
+++ app/components/get-weather.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {getWeather, getForcast} from '../apis/open-weather';
 
-export default function GetWeather({className}) {
-  return <span className={className}>
-    <span>enter a city and state</span>
-    <input type="text"></input>
-    <button>get weather</button>
-  </span>;
+export default class GetWeather extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({location: event.target.value});
+  }
+
+  handleClick(location) {
+    getWeather(location).then(response => console.log(response));
+    //getForcast(location).then(response => console.log(response));
+  }
+
+  render() {
+    return <span className={this.props.className}>
+      <span>enter a city and state</span>
+      <input type="text" value={this.state.location}
+        onChange={this.handleChange}></input>
+      <button onClick={this.handleClick.bind(null, this.state.location)}>get weather</button>
+    </span>;
+  }
 }
