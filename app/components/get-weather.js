90de30b4e65aaeaf988b4cc3e7aefd95d6e42e7a
bin lu <lu.bin.lu.bin@gmail.com>
2017-05-10 11:10:09 -0400

    resue GetWeather

--- app/components/get-weather.js
+++ app/components/get-weather.js
@@ -2,19 +2,8 @@ import React from 'react';
 
 export default function GetWeather({className}) {
   return <span className={className}>
-    <div>
-      <p>enter a city and this.state
-      </p>
-    </div>
-    <div>
-      <p>
-        <input type='text'/>
-      </p>
-    </div>
-    <div>
-      <p>
-        <input type='submit' value='get weather'/>
-      </p>
-    </div>
+    <span>enter a city and state</span>
+    <input type="text"></input>
+    <button>get weather</button>
   </span>;
 }
--- app/components/header.js
+++ app/components/header.js
@@ -2,8 +2,5 @@ import React from 'react';
 import GetWeather from './get-weather';
 
 export default function Header() {
-  return <div className='header'>
-    header
-    <GetWeather />
-  </div>;
+  return <GetWeather className="header" />;
 }
