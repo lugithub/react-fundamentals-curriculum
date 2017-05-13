83c9cb0b2543df89ec49f4579a3e64636eac16df
lubin <lu.bin.lu.bin@gmail.com>
2017-05-13 00:09:21 -0400

    step 6

--- app/components/forecast.js
+++ app/components/forecast.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import Header from './header';
+import queryString from 'query-string';
 
-function Forecast() {
-  return <div>
-    <Header />
-    <span>forecast component</span>
-  </div>
+import Header from './header';
+import {getWeather, getForcast} from '../apis/open-weather';
+
+class Forecast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState(() => ({loading: true}));
+    const {search} = this.props.location;
+    const searchObj = queryString.parse(search);
+    getForcast(searchObj.city)
+      .then(response => console.log(response))
+      .then(() => setTimeout(() =>
+        this.setState(() => ({loading: false})), 3000));
+  }
+
+  render() {
+    return <div>
+      <Header />
+      <span>{this.state.loading ? 'loading...' : 'forecast component'}</span>
+    </div>
+  }
 }
 export default Forecast;
